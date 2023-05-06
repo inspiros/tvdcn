28f1b9adfe5041e5f3ba065e9d85b4c635766955
inspiros <inspiros.tran@gmail.com>
2023-05-07 06:45:37 +0700

    again

--- tools/test_install_cuda_linux.sh
+++ tools/test_install_cuda_linux.sh
@@ -14,8 +14,8 @@ set -x
 #    libcublas-devel-11-8-11.11.3.6-1
 #ln -s cuda-11.8 /usr/local/cuda
 
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-yum install --setopt=obsoletes=0 -y \
+add-apt-repository https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+sudo apt-get install -y \
     cuda-nvcc-11-2-11.2.152-1 \
     cuda-cudart-devel-11-2-11.2.152-1 \
     libcurand-devel-11-2-10.2.3.152-1 \
