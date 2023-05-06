f8381a328d77d71b031690c30c6a071c2dff59eb
inspiros <inspiros.tran@gmail.com>
2023-05-07 06:12:36 +0700

    again

--- tools/prepare_build_environment_linux.sh
+++ tools/prepare_build_environment_linux.sh
@@ -6,11 +6,20 @@ set -x
 # Install CUDA 11.8, see:
 # * https://gitlab.com/nvidia/container-images/cuda/-/blob/master/dist/11.2.2/centos7-x86_64/base/Dockerfile
 # * https://gitlab.com/nvidia/container-images/cuda/-/blob/master/dist/11.2.2/centos7-x86_64/devel/Dockerfile
+#yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+#yum install --setopt=obsoletes=0 -y \
+#    cuda-nvcc-11-8-11.8.89-1 \
+#    cuda-cudart-devel-11-8-11.8.89-1 \
+#    libcurand-devel-11-8-10.3.0.86-1 \
+#    libcublas-devel-11-8-11.11.3.6-1
+#ln -s cuda-11.8 /usr/local/cuda
+
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum install --setopt=obsoletes=0 -y \
-    cuda-nvcc-11-8-11.8.89-1 \
-    cuda-cudart-devel-11-8-11.8.89-1 \
-    libcurand-devel-11-8-10.3.0.86-1 \
-    libcublas-devel-11-8-11.11.3.6-1
-ln -s cuda-11.8 /usr/local/cuda
+    cuda-nvcc-11-2-11.2.152-1 \
+    cuda-cudart-devel-11-2-11.2.152-1 \
+    libcurand-devel-11-2-10.2.3.152-1 \
+    libcudnn8-devel-8.1.1.33-1.cuda11.2 \
+    libcublas-devel-11-2-11.4.1.1043-1
+ln -s cuda-11.2 /usr/local/cuda
 export CUDA_HOME="/usr/local/cuda"
