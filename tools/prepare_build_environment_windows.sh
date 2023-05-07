ec69d79ab293b0f95d831bc1e7e7d1bfc11b99cb
inspiros <inspiros.tran@gmail.com>
2023-05-07 09:01:42 +0700

    again

--- tools/prepare_build_environment_windows.sh
+++ tools/prepare_build_environment_windows.sh
@@ -5,12 +5,12 @@ set -x
 
 # Install CUDA 11.8:
 echo "Installing CUDA 11.8"
-curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_522.06_windows.exe
+curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/network_installers/cuda_11.8.0_windows_network.exe
 ./cuda.exe -s nvcc_11.8 cudart_11.8 cublas_dev_11.8 curand_dev_11.8 cusolver_dev_11.8 cusparse_dev_11.8
 rm cuda.exe
 
 export PATH="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8/bin":$PATH
-export CUDA_HOME="/c/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
+export CUDA_HOME="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
 
 # test
 nvcc -V
