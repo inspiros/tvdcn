89103e38876f29d8c9a199367341c302073cd2b1
inspiros <inspiros.tran@gmail.com>
2023-05-07 07:51:41 +0700

    again

--- tools/prepare_build_environment_windows.sh
+++ tools/prepare_build_environment_windows.sh
@@ -3,8 +3,9 @@
 set -e
 set -x
 
+# Install CUDA 11.8:
 curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_522.06_windows.exe
-./cuda.exe -s nvcc_11.8 cudart_11.8 curand_dev_11.8 cublas_dev_11.8 cusparse_dev_11.8
+./cuda.exe -s nvcc_11.8 cudart_11.8 cublas_dev_11.8 curand_dev_11.8 cusolver_dev_11.8 cusparse_dev_11.8
 rm cuda.exe
 export CUDA_HOME="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.8"
 
