fix wrong dimension checking for offset and mask

tvdcn/ops/deform_conv.py
@@ -275,7 +275,7 @@ def deform_conv3d(
         >>> # In this case, for an input of 10, stride of 1 and kernel size of 3,
         >>> # without padding, the output size is 8.
         >>> offset = torch.rand(1, 3 * kd * kh * kw, 8, 8, 8)
-        >>> mask = torch.rand(1, kd * kh * kw, 8, 8, 8)
+        >>> mask = torch.rand(1, kd * kh * kw, 8, 8, 8).sigmoid()
         >>> out = deform_conv3d(input, weight, offset, mask)
         >>> print(out.shape)
         Output:

tvdcn/ops/deform_conv_transpose.py
@@ -275,7 +275,7 @@ def deform_conv_transpose3d(
         >>> weight = torch.rand(3, 5, kd, kh, kw)
         >>> # offset and mask should have the same spatial size as the input.
         >>> offset = torch.rand(1, 3 * kd * kh * kw, 8, 8, 8)
-        >>> mask = torch.rand(1, kd * kh * kw, 8, 8, 8)
+        >>> mask = torch.rand(1, kd * kh * kw, 8, 8, 8).sigmoid()
         >>> out = deform_conv_transpose3d(input, weight, offset, mask)
         >>> print(out.shape)
         Output: