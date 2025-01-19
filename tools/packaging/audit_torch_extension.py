4a03dfc7a807d09370c66ab6fad13b2477a76f80
Hoang-Nhat Tran <hnhat.tran@gmail.com>
2025-01-19 20:33:36 +0100

    update audit_torch_extension script

--- tools/packaging/audit_torch_extension.py
+++ tools/packaging/audit_torch_extension.py
@@ -1,17 +1,27 @@
 import sys
 
 from auditwheel.main import main
-from auditwheel.policy import _POLICIES as POLICIES
 
-for p in POLICIES:
-    p['lib_whitelist'].extend([
-        'libtorch.so',
-        'libtorch_cpu.so',
-        'libtorch_cuda.so',
-        'libtorch_python.so',
-        'libc10.so',
-        'libc10_cuda.so',
-    ])
+
+LIBTORCH_LIBRARIES = [
+    'libtorch.so',
+    'libtorch_cpu.so',
+    'libtorch_cuda.so',
+    'libtorch_python.so',
+    'libc10.so',
+    'libc10_cuda.so',
+]
+
+try:
+    from auditwheel.policy import _POLICIES as POLICIES
+
+    for p in POLICIES:
+        p['lib_whitelist'].extend(LIBTORCH_LIBRARIES)
+except ImportError:
+    for lib in LIBTORCH_LIBRARIES:
+        sys.argv.append('--exclude')
+        sys.argv.append(lib)
+
 
 if __name__ == "__main__":
     sys.exit(main())
