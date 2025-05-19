3f8e1df36a10025a23c3f55660e91571fc4893a2
Hoang-Nhat Tran <hnhat.tran@gmail.com>
2025-05-19 16:18:12 +0200

    fix curl command on windows

--- tools/packaging/prepare_build_environment_windows.sh
+++ tools/packaging/prepare_build_environment_windows.sh
@@ -5,7 +5,7 @@ set -x
 
 # Install CUDA 12.8: (requires thrust)
 echo "Installing CUDA 12.8"
-curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/network_installers/cuda_12.8.1_windows_network.exe
+curl -L -v -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/network_installers/cuda_12.8.1_windows_network.exe
 ./cuda.exe -s nvcc_12.8 cudart_12.8 thrust_12.8 cublas_dev_12.8 curand_dev_12.8 cusolver_dev_12.8 cusparse_dev_12.8
 rm cuda.exe
 
